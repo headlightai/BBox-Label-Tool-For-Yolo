d6e7d57498e5be7a51f426d9ecca79e870d5a8f7
Mahdi89 <jelodari.m@gmail.com>
2019-04-08 11:15:42 +0100

    Dont mess up the file orders.

--- utils/rename-to-num.py
+++ utils/rename-to-num.py
@@ -1,11 +1,13 @@
 import os
 import glob
 
-path = '.'
-
-cnt = 0
-for filename in glob.glob(os.path.join(path, '*.jpg')):
-  	base = os.path.splitext(filename)
-  	num = os.path.splitext(base[0])
-  	os.rename(filename, str(cnt) + ".jpg")
-	cnt = cnt + 1
+directory = './'
+lst = glob.glob("./*.jpg")
+for i in lst:
+  	base = os.path.splitext(i)
+        print base
+  	num = os.path.splitext(base[0])[1]
+        number = int(num[1:])
+        target = os.path.join(directory, i[2:])
+        current = os.path.join(directory, str(number) + '.jpg')
+  	os.rename(target, current)
